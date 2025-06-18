2994cfcde4aa81184131e9a83a7c7f887b0b0584
AnahiN22 <95322773+AnahiN22@users.noreply.github.com>
2025-06-18 04:12:41 -0300

    Add files via upload

---
+++ Árbol sintáctico en Prolog/prolog2.pl
@@ -0,0 +1,17 @@
+o --> o(_).
+o(o(SN, SV)) --> sn(SN, _Gen, Num), sv(SV, Num).
+sn(sn(DET, N), Gen, Num) --> det(DET, Gen, Num), n(N, Gen, Num).
+sv(sv(VT, SN), Num) --> vt(VT, Num), sn(SN, _Gen, _Num).
+sv(sv(VI), Num) --> vi(VI, Num).
+det(det(X), f, sg) --> [X], {member(X, [la, una])}.
+det(det(X), f, pl) --> [X], {member(X, [las, unas])}.
+det(det(X), m, sg) --> [X], {member(X, [el, un])}.
+det(det(X), m, pl) --> [X], {member(X, [los, unos])}.
+vt(vt(cobra), sg) --> [cobra].
+vt(vt(cobran), pl) --> [cobran].
+vi(vi(trabaja), sg) --> [trabaja].
+vi(vi(trabajan), pl) --> [trabajan].
+n(n(empleada), f, sg) --> [empleada].
+n(n(empleadas), f, pl) --> [empleadas].
+n(n(X), m, sg) --> [X], {member(X, [empleado, sueldo])}.
+n(n(X), m, pl) --> [X], {member(X, [empleados, sueldos])}.
